c7ef88ea88fdcf075c19204ea210396febf4f10f
ethika-snolan <snolan@ethika.com>
2024-06-13 10:49:52 -0700

    - nothing spa related, just stuff

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { error, data } = useFetch( 'https://jsonplaceholder.typicode.com/todos/1' )
+console.log( 'error', error )
+console.log( 'data', data )
+</script>
+
+<template>
+  <h1>error...</h1>
+  <p>Error: {{ error?.message }}</p>
+  <p>Data: {{ data?.title }}</p>
+</template>
+
+<style scoped>
+
+</style>
